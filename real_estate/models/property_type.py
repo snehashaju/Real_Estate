efdf2125eb2ad950c9470034b85d86289f11844c
snehashaju <idigi.sneha@gmail.com>
2023-05-06 08:59:16 +0530

    [ADD] Real Estate added new module

--- real_estate/models/property_type.py
+++ real_estate/models/property_type.py
@@ -12,7 +12,15 @@ class PropertyType(models.Model):
         ('name', 'unique (name)', 'The Property Type must be unique !')
     ]
 
-
+    def property_button(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Offer',
+            'view_mode': 'list,form',
+            'res_model': 'estate.property.offer',
+            'domain': "[('property_type_id','in', 'name')]" ,
+            'target': "current"
+        }
 
 class PropertyRelated(models.Model):
     _name = 'estate.related'
@@ -21,4 +29,6 @@ class PropertyRelated(models.Model):
     property_id = fields.Many2one('estate.property',string='Property', domain="[('property_type_id','=', property_type_id)]")
     expected_price = fields.Float(string='Expected Price', related='property_id.expected_price')
     status_text = fields.Selection(string='Status', related= 'property_id.status_text')
+
+
 
